381660e376638726a583a6a05fb7cefba5603a3a
T-107 <T-107@PC185>
2017-11-21 21:43:46 -0600

    REST

---
+++ src/main/java/unitec/org/mongounitec/Estatus.java
@@ -0,0 +1,17 @@
+
+package unitec.org.mongounitec;
+
+/*
+ * @author T-107
+ */
+public class Estatus {
+    private boolean success;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+}
